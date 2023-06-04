3555df9c5aa3f532d9479ab6e0e03268c630e828
kyemets <yemetskonstantin@gmail.com>
2023-06-04 15:48:55 +0200

    added search

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <h1>Page with posts</h1>
+    <my-input 
+      v-model="searchQuery"
+      placeholder="Search..."
+    />
     <div class="app__btns">
       <my-button
       @click="showDialog"
@@ -19,7 +23,7 @@
     </my-dialog>
 
     <post-list 
-      :posts="sortedPosts"
+      :posts="sortedAndSearchedPosts"
       @remove="removePost"
       v-if="!isPostsLoading"
     />
@@ -51,6 +55,7 @@ export default {
       dialogVisible: false,
       isPostsLoading: false,
       selectedSort: '',
+      searchQuery: '',
       sortOptions: [
         {value: 'title', name: 'By name'},
         {value: 'body', name: 'By description'}
@@ -91,6 +96,9 @@ export default {
     computed: {
       sortedPosts() {
         return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+      },
+      sortedAndSearchedPosts() {
+        return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
       }
     },
     watch: {
